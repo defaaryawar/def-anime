dc5c9e7226ac20be97a7b8e7e5758eb4b81197b3
Defaaryawar <defadefa1313@gmail.com>
2024-12-06 00:50:45 +0700

    perbaikan header buat theme

--- src/components/AnimeList/Header.jsx
+++ src/components/AnimeList/Header.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 const Header = ({title, LinkHref, LinkTitle}) => {
     return (
         <div className="flex justify-between items-center p-2">
-            <h1 className="px-2 pt-2 font-bold text-xl select-none font-arial text-base-300">{title}</h1>
+            <h1 className="px-2 pt-2 font-bold text-xl select-none font-arial">{title}</h1>
             {LinkHref && LinkTitle ?
-             <Link href={LinkHref} className="md:text-lg text-sm hover:text-colorPrimarySaya transition-all mr-2 text-base-300">{LinkTitle}
+             <Link href={LinkHref} className="md:text-lg text-sm hover:text-colorPrimarySaya transition-all mr-2">{LinkTitle}
              </Link>
              : null
             }

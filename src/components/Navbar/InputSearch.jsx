6ebe2c80a28e08f445a9ec6ec81bccf6deb0905a
Defaaryawar <defadefa1313@gmail.com>
2024-12-06 01:17:55 +0700

    perbaikan padding

--- src/components/Navbar/InputSearch.jsx
+++ src/components/Navbar/InputSearch.jsx
@@ -32,9 +32,9 @@ const InputSearch = () => {
     };
 
     return (
-        <div className="flex items-center join px-4 bg-transparent">
+        <div className="flex items-center join px-4 bg-transparent bg-opacity-55">
             <input
-                className={`px-4 py-2 w-full rounded-md border ${isValid ? 'border-gray-300' : 'border-red-500'} focus:outline-none focus:ring-2 focus:ring-colorPrimarySaya focus:border-transparent join-item transition-all duration-300 outline-none bg-base-300`}
+                className={`px-4 py-2 w-full rounded-md border ${isValid ? 'border-gray-300' : 'border-red-500'} focus:outline-none focus:ring-2 focus:ring-colorPrimarySaya focus:border-transparent join-item transition-all duration-300 outline-none bg-base-300 bg-opacity-65`}
                 placeholder="Search"
                 ref={searchRef}
                 onKeyDown={handleSearch}
